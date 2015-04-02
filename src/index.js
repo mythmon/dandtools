a4606d1fa51b875014490bd380ca0ada65191a05
Mike Cooper <mythmon@gmail.com>
2015-04-01 23:45:41 -0700

    Add and remove components.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import * as dice from './dice';
+import {getId} from './utils';
 
 
 class Main extends React.Component {
   render() {
     return (
       <div className="Container">
-        <DiceRoller/>
-        <DiceRoller/>
+        <Selector/>
       </div>
     );
   }
 }
+
 
 class DiceRoller extends React.Component {
   constructor(props) {
@@ -38,17 +39,66 @@ class DiceRoller extends React.Component {
   render() {
     var valid = dice.isValid(this.state.diceSpec);
     return (
-      <section className="DiceRoller">
-        <div className="row">
-          <input className="DiceRoller__input" type="text" value={this.state.diceSpec} onChange={this.handleTyping.bind(this)}/>
-          <button onClick={this.roll.bind(this)} disabled={!valid}>Roll</button>
+      <div className="Section DiceRoller">
+        <div className="Section__title">
+          Dice Roller
+          <button className="Section__close small" onClick={() => this.props.remove(this)}>X</button>
         </div>
-        <div className="row">
-          <span className="DiceRoller__output">
-            Results: {this.state.results.join(', ')}
-          </span>
+        <div className="Section__body">
+          <div className="row">
+            <input className="DiceRoller__input" type="text" value={this.state.diceSpec} onChange={this.handleTyping.bind(this)}/>
+            <button onClick={this.roll.bind(this)} disabled={!valid}>Roll</button>
+          </div>
+          <div className="row">
+            <span className="DiceRoller__output">
+              Results: {this.state.results.join(', ')}
+            </span>
+          </div>
         </div>
-      </section>
+      </div>
+    );
+  }
+}
+
+
+class Selector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      components: [],
+    };
+  }
+
+  addComponent(componentName) {
+    let {components} = this.state;
+    let id = `component-${componentName}-${getId()}`;
+    components.push([id, componentName]);
+    this.setState({components});
+  }
+
+  removeComponent(idToRemove) {
+    let {components} = this.state;
+    components = components.filter(([id, c]) => id !== idToRemove);
+    this.setState({components});
+  }
+
+  render() {
+    return (
+      <div className="Selector">
+        <div className="Selector__choices">
+          Add component:
+          <button onClick={this.addComponent.bind(this, 'DiceRoller')}>DiceRoller</button>
+        </div>
+        <div className="Selector__active">
+          {this.state.components.map(([id, componentName]) => {
+            if (componentName === 'DiceRoller') {
+              return <DiceRoller key={id} remove={this.removeComponent.bind(this, id)}/>;
+            } else {
+              throw `Unknown component ${componentName}`;
+            }
+          })}
+        </div>
+      </div>
     );
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,9 @@
 export function randInt(a, b) {
   return Math.floor(Math.random() * (b - a + 1) + a);
 }
+
+let nextId = 1;
+export function getId() {
+  nextId += 1;
+  return nextId;
+}
